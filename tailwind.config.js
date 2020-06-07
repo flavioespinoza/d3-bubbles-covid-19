3d5fed7a12e6f993355bc5a7dddef86d390b2313
flavioespinoza <flavio.espinoza@gmail.com>
2020-06-07 12:45:41 -0600

    indigo

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,26 @@ module.exports = {
       display: ['Roboto', 'sans-serif'],
       body: ['Roboto', 'sans-serif'],
     },
-    colors: colors,
+    colors: {
+      indigo: {
+        lighter: '#b3bcf5',
+        default: '#5c6ac4',
+        dark: '#202e78',
+      },
+      pink: {
+        dark: '#6f164a',
+        light: '#eb3981',
+        lighter: '#fef3f8',
+      },
+      grey: {
+        darkest: '#232323',
+        darker: '#464646',
+        dark: '#6d6d6d',
+      },
+    },
     extend: {
       colors: {
-        pink: {
-          dark: '#6f164a',
-          light: '#eb3981',
-          lighter: '#fef3f8',
-        },
-        grey: {
-          darkest: '#232323',
-          darker: '#464646',
-          dark: '#6d6d6d',
-        },
+
       },
       margin: {
         '96': '24rem',
