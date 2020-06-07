98ad58be36e8d028778751522cfc4fc472c4e82e
flavioespinoza <flavio.espinoza@gmail.com>
2020-06-07 12:31:08 -0600

    tailwind_config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,20 @@ module.exports = {
   important: true,
   theme: {
     fontFamily: {
-      display: ['Gilroy', 'sans-serif'],
-      body: ['Graphik', 'sans-serif'],
+      display: ['Roboto', 'sans-serif'],
+      body: ['Roboto', 'sans-serif'],
     },
     extend: {
       colors: {
+        black: '#171e21',
         cyan: '#9cdbff',
+        pink: '#e81b74',
+        'pink-dark': '#6f164a',
+        'pink-light': '#eb3981',
+        'pink-lighter': '#fef3f8',
+        indigo: '#5c6ac4',
+        blue: '#00a6cc',
+        red: '#de3618',
       },
       margin: {
         '96': '24rem',
