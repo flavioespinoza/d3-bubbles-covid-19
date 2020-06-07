01f64a84a21e54ed7939821015aa2c05886baba0
flavioespinoza <flavio.espinoza@gmail.com>
2020-06-07 12:37:53 -0600

    tailwind_config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,27 @@ module.exports = {
       display: ['Roboto', 'sans-serif'],
       body: ['Roboto', 'sans-serif'],
     },
+    colors: {
+      black: '#171e21',
+      cyan: '#9cdbff',
+      pink: '#e81b74',
+      indigo: '#5c6ac4',
+      blue: '#00a6cc',
+      red: '#de3618',
+      grey: '#969696'
+    },
     extend: {
       colors: {
-        black: '#171e21',
-        cyan: '#9cdbff',
-        pink: '#e81b74',
-        'pink-dark': '#6f164a',
-        'pink-light': '#eb3981',
-        'pink-lighter': '#fef3f8',
-        indigo: '#5c6ac4',
-        blue: '#00a6cc',
-        red: '#de3618',
+        pink: {
+          dark: '#6f164a',
+          light: '#eb3981',
+          lighter: '#fef3f8',
+        },
+        grey: {
+          darkest: '#232323',
+          darker: '#464646',
+          dark: '#6d6d6d',
+        },
       },
       margin: {
         '96': '24rem',
